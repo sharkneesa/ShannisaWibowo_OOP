b4a8a6221f9913c0983aaa3aab6b43f6a43e43b2
Shannisa <shanniisaa@gmail.com>
2026-09-04 10:50:41 +0700

    ShannisaWibowo - init

--- Frontend_Shannisa/core/src/main/java/com/Shannisa/frontend/Enemy.java
+++ Frontend_Shannisa/core/src/main/java/com/Shannisa/frontend/Enemy.java
@@ -1,17 +1,49 @@
 package com.Shannisa.frontend;
 
-public static class Enemy {
+public class Enemy {
     String name;
     int hp;
     int maxHp;
 
-    public Enemy(String name, int hp, int maxHp){
+    public Enemy(String name, int hp){
         this.name=name;
         this.hp=hp;
         this.maxHp=hp;
     }
-}
 
-public static void main(String[] args){
-    Enemy name = new Enemy("Reimu Hakurei", 100, 50);
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        hp=hp-damage;
+        // 2. HP must not go below 0.
+        if (hp<0){
+            hp=0;
+        }
+        // 3. Display the current HP in the format: [EnemyName] took [damage] damage! HP: [currentHP]/[maxHP]
+        System.out.println(name + "took" + damage + "damage!" + "Remaining HP:" + hp + "/" + maxHp);
+        // 4. If HP reaches 0, display that the Enemy has been defeated, in the format: [EnemyName] was defeated!
+        if (hp==0){
+            System.out.println(name + "was defeated!");
+        }
+    }
+
+    public void attack(Player player, int damage) {
+        // 1. Display information that the Enemy is attacking the Player, in the format: [EnemyName] unleashes bullet barrage on [PlayerName]!
+
+        System.out.println(name + "unleashes bullet barrage on" + player.name);
+
+        // 2. Call the Player's takeDamage() method using the given damage.
+
+        takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if (hp>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
--- Frontend_Shannisa/core/src/main/java/com/Shannisa/frontend/Player.java
+++ Frontend_Shannisa/core/src/main/java/com/Shannisa/frontend/Player.java
@@ -14,4 +14,53 @@ public class Player {
         this.spellCards=spellCards;
     }
 
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+
+        hp=hp-damage;
+
+        // 2. HP must not become negative.
+
+        if (hp<0){
+            hp=0;
+        }
+
+        // 3. If HP is still greater than 0, display the remaining HP in the format: [PlayerName] took [damage] damage! Remaining HP: [hp]
+
+        // 4. If HP reaches 0, display a message that the Player has been defeated.
+
+        if (hp>0){
+            System.out.println(name + "took" + damage + "damage!" + "Remaining HP:" + hp);
+        }
+        else if (hp==0) {
+            System.out.println("Player has been defeated");
+        }
+
+    }
+
+    public void shoot(Enemy target) {
+        // 1. Create an int named damage, calculated by adding 10 to power.
+
+        int damage=power+10;
+
+        // 2. Display information that the Player is shooting the Enemy, in the format: [name] shoots [TargetName] dealing [damage] DMG!
+
+        System.out.println(name + "shoots" + target + "dealing" + damage + "DMG!");
+
+        // 3. Call the Enemy object's takeDamage() method.
+
+        target.takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if (hp>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
 }
